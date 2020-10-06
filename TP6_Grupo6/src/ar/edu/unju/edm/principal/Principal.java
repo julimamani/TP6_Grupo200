253c8b9fd1676e06a38d3a852dd59c608d41242a
Julieta Mamani <Julieta Mamani@LAPTOP-9IA3U065>
2020-10-06 17:38:35 -0300

    principal

---
+++ TP6_Grupo6/src/ar/edu/unju/edm/principal/Principal.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.edm.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ing = new Scanner(System.in);
+		System.out.println("Ingrese 3 lados");
+		System.out.println("Lado A: ");
+		ing.nextInt();
+		System.out.println("Lado B");
+		ing.nextInt();
+		System.out.println("Lado C");
+		ing.nextInt();
+	}
+
+}
